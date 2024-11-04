53fbfed0487e95c2ae50a9676399b663984d3981
unknown <aaryan.mishra.soton@gmail.com>
2024-11-04 13:50:21 +0000

    added my name

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
+        System.out.println("Hello from Aaryan!")
     }
 
     public static String sayHello(){
